c5c2d9baedece3e8602d9ebbe4c4ac74c760177c
Briuwu <millontebrian@gmail.com>
2024-07-16 16:47:40 +0800

    chore: Update layout and styling for stages page

--- app/stages/page.tsx
+++ app/stages/page.tsx
@@ -2,10 +2,10 @@ import { Guidebook } from "./components/guidebook";
 
 const StagesPage = () => {
   return (
-    <section className="container">
+    <section className="">
       <div>
         <div className="bg-light-green text-black p-4 lg:p-8 flex items-center justify-between rounded-md border-2 border-black">
-          <h1 className="font-bold text-xl uppercase md:text-2xl lg:text-3xl">
+          <h1 className="font-bold text-base uppercase md:text-2xl lg:text-3xl">
             Stage 1 - HTML
           </h1>
           <Guidebook />
